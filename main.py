717067a8fd20655e0ce88307b00d253e71dbaf7d
joshdeansavv <115428326+joshdeansavv@users.noreply.github.com>
2025-07-14 22:52:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ load_env_file()
 
 # Configuration
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = {GUILD_ID_GOES_HERE}
-VOICE_CHANNEL_ID = {VOICE_CHANNEL_ID_GOES_HERE}
+GUILD_ID = {GUILD_ID_HERE}
+VOICE_CHANNEL_ID = {VOICE_CHANNEL_ID_HERE}
 STREAM_URL = "https://audio.junctionnow.com:8000/radio.mp3"
 
 # Validate token
